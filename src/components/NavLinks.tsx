7bceaf33ab5ebd4c8d03de499b6c48f6108bea66
MehmetZiya <mehmetziya82@gmail.com>
2022-10-20 14:21:41 +0200

    adding active NavLink styles

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -2,6 +2,9 @@ import { NavLink } from 'react-router-dom'
 
 //styles
 import classes from '../css/Navbar.module.css'
+let activeStyle = {
+  textDecoration: 'underline',
+}
 
 type Props = {
   handleLinkClick: () => void
@@ -10,14 +13,11 @@ type Props = {
 const NavLinks: React.FC<Props> = ({ handleLinkClick }) => {
   return (
     <>
-      <NavLink onClick={handleLinkClick} to='/' className={classes.navItem}>
-        Home
-      </NavLink>
-
       <NavLink
         onClick={handleLinkClick}
         to='/popular'
         className={classes.navItem}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
       >
         Popular Films
       </NavLink>
@@ -26,6 +26,7 @@ const NavLinks: React.FC<Props> = ({ handleLinkClick }) => {
         onClick={handleLinkClick}
         to='/now_playing'
         className={classes.navItem}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
       >
         Now Playing
       </NavLink>
@@ -34,6 +35,7 @@ const NavLinks: React.FC<Props> = ({ handleLinkClick }) => {
         onClick={handleLinkClick}
         to='/top_rated'
         className={classes.navItem}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
       >
         Top Rated
       </NavLink>
@@ -42,6 +44,7 @@ const NavLinks: React.FC<Props> = ({ handleLinkClick }) => {
         onClick={handleLinkClick}
         to='/genres'
         className={classes.navItem}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
       >
         Genres
       </NavLink>
